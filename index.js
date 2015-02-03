f77a50605a1db8d5a95e9a239d99b1205fc79e53
StephanieBeaton <stefanybeaton@gmail.com>
2015-02-02 18:10:53 -0800

    extra points command line utility

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
 
 var greeting = require('./lib/greeting.js');
 
-console.log(greeting.greet('sam'));
+//console.log(greeting.greet('sam'));
+
+process.argv.forEach(function(arg, index) {
+  // console.log(index + ' = ' + arg);
+  if (index == '2') {
+    console.log(greeting.greet('sam'));
+  }
+});
